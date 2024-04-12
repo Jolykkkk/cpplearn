4f6ffcdb6f7c9781e089019804e9151f7ab94b1a
zkc <18772668057@163.com>
2024-04-12 21:51:05 +0800

    chapter 12 learning

---
+++ chapter/chapter12/12_1_vegnews.cpp
@@ -0,0 +1,33 @@
+////
+//// Created by zkc on 2024/3/27.
+////
+//#include <iostream>
+//#include "stringbad.h"
+//using namespace std;
+//
+//void callme1(StringBad &rsb);
+//void callme2(StringBad sb);
+//
+////int main()
+//    StringBad headline1("hello world");
+//    StringBad headline2("all in bear");
+//    StringBad sports("ball");
+//
+//    std::cout << headline1 << endl;
+//    std::cout << headline2 << endl;
+//    std::cout << sports << endl;
+//
+//    callme1(headline1);
+//    callme2(headline2);//
+////    headline2 传递给 sb 但是构造函数中没有对应匹配的项 自动生成复制构造函数
+//}
+//
+//void callme1(StringBad &rsb){
+//    cout << "String passed by reference: " << rsb <<endl;
+//
+//}
+//
+//void callme2(StringBad sb){
+//    cout << "String passed by value: " << sb <<endl;
+//
+//}
